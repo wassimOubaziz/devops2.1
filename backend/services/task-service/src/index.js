fce52e4beb38bb4dc51858616893f6eb5d30508e
Wassim Oubaziz <wassimdjaafar5@gmail.com>
2024-12-17 18:26:42 +0100

    hi 3

--- backend/services/task-service/src/index.js
+++ backend/services/task-service/src/index.js
@@ -9,7 +9,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: '*',
+  origin: ['*', 'localhost:3000', 'http://localhost:3000'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
